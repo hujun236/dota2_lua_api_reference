a5e5003e40711e7dfdc9ca34376bbd0dee523999
hujun236 <hujun236@qq.com>
2020-12-14 01:40:30 +0800

    test

--- GenerateEmmyLuaAnnotations.lua
+++ GenerateEmmyLuaAnnotations.lua
@@ -13,11 +13,28 @@ end
 
 local allData = json.decode(fullString)
 
+function saveClass(data)
+    local path = targetDir .. "/" .. data.name .. ".lua"
+
+    local fieldsStrings = {}
+    if data.members then
+        for i,v in ipairs(data.members) do
+            if v.kind == "field" then
+                local string = "--"..v.description.."\r\n"
+                string = string .. "\t---@type " .. table.concat( v.types, "|") .. "\r\n";
+                stirng = string .. "\t" .. v.name .. " = nil;\r\n"
+                fieldsStrings[#fieldsStrings+1] = string;
+            end
+        end
+    end
+end
+
 for i,v in ipairs(allData) do
     if v.kind == "class" then
-        
+        saveClass(v)
     end
 
-
 end
+
+
 
